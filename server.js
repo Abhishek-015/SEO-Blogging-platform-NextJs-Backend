b03605c2cd0f241b1e471ca58a4df53dd2546901
Abhishek Singh <abhishek404singh@gmail.com>
2022-04-19 17:51:57 +0530

    connected app with database(mongodb) and setup routes for testing

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
-const express  = require("express")
-const morgan  = require("morgan")
-const bodyParser  = require("body-parser")
-const cookieParser  = require("cookie-parser")
-const cors  = require("cors")
-require("dotenv").config()
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const blogRoutes = require('./routes/blog')
 
 //app
-const app= express()   //loading express app features on app variable
+const app = express(); //loading express app features on app variable
+
+//Database
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected"));
 
 //middlewares--->adding middlewares to our app
-app.use(morgan("dev"))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-//routes
-app.get('/api',(req,res)=>{
-    res.json({time:Date().toString()})
-})
+//cors----->It is also a middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+//routes middleware
+app.use("/api",blogRoutes)
+
 
 //port
-const port = process.env.PORT || 8000
-app.listen(port,()=>{
-    console.log(`server is listening at port ${port}`)
-})
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`server is listening at port ${port}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
