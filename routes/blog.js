2294c8bfba4faa6348cffda6636849d2e9c69e7b
Abhishek Singh <abhishek404singh@gmail.com>
2022-04-19 18:38:19 +0530

    start controllers and routes implemntaion

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.json({time:Date().toString()})
-});
+//controllers
+const {time} = require("../controllers/blog.js")
+
+router.get('/',time);
 
 module.exports=router;
