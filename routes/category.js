7b6480b9dc5db26ddafab91e71567aa4dc98e5d5
Abhishek Singh <abhishek404singh@gmail.com>
2022-05-03 17:12:23 +0530

    created category model and category contrroller for handljng end points and implemented error handler function to get the desred error instead of error object

---
+++ routes/category.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { create } = require("../controllers/category");
+
+//validators
+const { runValidation } = require("../validators");
+const { categoryCreateValidator } = require("../validators/category");
+
+//middlewares
+const { requireSignin, adminMiddleWare } = require("../controllers/auth");
+
+router.post(
+  "/category",
+  categoryCreateValidator,
+  runValidation,
+  requireSignin,
+  adminMiddleWare,
+  create
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 const blogRoutes = require("./routes/blog");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 //app
 const app = express(); //loading express app features on app variable
@@ -35,6 +36,7 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api", blogRoutes);
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 //port
 const port = process.env.PORT || 8000;
